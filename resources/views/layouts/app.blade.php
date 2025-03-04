66431c11f4812e5aee03e0e73ae04c0f4da940f4
leartpro <70581914+leartpro@users.noreply.github.com>
2025-03-04 22:32:58 +0100

    make 'php artisan serve' possible again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,54 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark:bg-slate-700">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
 
-        @livewireStyles
+    @livewireStyles
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        <script src="https://kit.fontawesome.com/fec4df1c10.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="font-sans antialiased">
-        <livewire:toasts />
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/fec4df1c10.js" crossorigin="anonymous"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-        {{-- <x-jet-banner /> --}}
-        {{-- <input type="text" id="datepicker"> --}}
+<body class="font-sans antialiased">
+<livewire:toasts/>
 
-        <div class="bg-white dark:bg-slate-700 w-full">
-            <div class="md:py-2 max-w-7xl sm:px-6 lg:px-8 mx-auto">
-                <livewire:user.navigation-top />
+{{-- <x-jet-banner /> --}}
+{{-- <input type="text" id="datepicker"> --}}
+
+<div class="bg-white dark:bg-slate-700 w-full">
+    <div class="md:py-2 max-w-7xl sm:px-6 lg:px-8 mx-auto">
+        <livewire:user.navigation-top/>
+    </div>
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-white dark:bg-slate-700">
+            <div class="py-1 md:py-1 max-w-7xl sm:px-6 lg:px-8 mx-auto">
+                {{ $header }}
             </div>
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-slate-700">
-                    <div class="py-1 md:py-1 max-w-7xl sm:px-6 lg:px-8 mx-auto">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        </header>
+    @endif
 
-            <!-- Page Content -->
-            <main>
-                <div class="max-w-7xl sm:px-6 lg:px-8 mx-auto">
-                    {{ $slot }}
-                </div>
-            </main>
+    <!-- Page Content -->
+    <main>
+        <div class="max-w-7xl sm:px-6 lg:px-8 mx-auto">
+            {{ $slot }}
         </div>
+    </main>
+</div>
 
-        @stack('modals')
-        @livewireScriptConfig
-    </body>
+@stack('modals')
+@livewireScriptConfig
+</body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: 'webportal.test',
+        https: true,
+        hmr: {
+            host: 'webportal.test',
+        },
+    },
 });
