5de2e948d86fe2e3cf78c58600839c431ae29d30
leartpro <70581914+leartpro@users.noreply.github.com>
2025-04-12 13:11:04 +0200

    server-side scripts and github action

---
+++ disable_debugbar.sh
@@ -0,0 +1,10 @@
+systemctl stop nginx
+systemctl stop php8.3-fpm
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+composer install --optimize-autoloader --no-dev
+npm run build
+systemctl start php8.3-fpm
+systemctl start nginx
---
+++ enable_debugbar.sh
@@ -0,0 +1,10 @@
+systemctl stop nginx
+systemctl stop php8.3-fpm
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+composer install
+npm run build
+systemctl start php8.3-fpm
+systemctl start nginx
