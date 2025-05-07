91c5f3311d4d4d4b17c3d9a4d78bfae61388f68f
leartpro <70581914+leartpro@users.noreply.github.com>
2025-05-07 15:43:51 +0200

    test environment and configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->app->environment() !== 'testing') {
+            $this->markTestSkipped('These tests must be run in testing environment to protect your database.');
+        }
+
+        if (config('database.connections.mysql.database') !== 'development') {
+            $this->markTestSkipped('Tests must use testing database, not production database.');
+        }
+
+    }
+
 }
