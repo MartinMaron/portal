02d468ff9b4bd9d694ae01bbdf14c688dda1ea3b
leartpro <70581914+leartpro@users.noreply.github.com>
2025-05-07 08:46:13 +0200

    Revert "project trim"
    This reverts commit 0485d96b5a8661f177235fa65544cbbdf709b2fb.

---
+++ dbaas-db-6448416/production/data/fueltypes.sql
@@ -0,0 +1,6 @@
+insert into production.fueltypes (id, einheit_id, hasTank, caption, created_at, updated_at) values ('EC4', 4, 0, 'Fernwärme', '2024-12-03 08:26:59', '2024-12-03 08:26:59');
+insert into production.fueltypes (id, einheit_id, hasTank, caption, created_at, updated_at) values ('EG1', 11, 1, 'Pellets', '2024-12-03 08:26:59', '2024-12-03 08:26:59');
+insert into production.fueltypes (id, einheit_id, hasTank, caption, created_at, updated_at) values ('GS4', 4, 0, 'Gas', '2024-12-03 08:26:59', '2024-12-03 08:26:59');
+insert into production.fueltypes (id, einheit_id, hasTank, caption, created_at, updated_at) values ('GS9', 9, 1, 'Flüssiggas', '2024-12-03 08:26:59', '2024-12-03 08:26:59');
+insert into production.fueltypes (id, einheit_id, hasTank, caption, created_at, updated_at) values ('OL9', 9, 1, 'Heizöl', '2024-12-03 08:26:59', '2024-12-03 08:26:59');
+insert into production.fueltypes (id, einheit_id, hasTank, caption, created_at, updated_at) values ('STZ', 4, 0, 'Strom', '2024-12-03 08:26:59', '2024-12-03 08:26:59');
