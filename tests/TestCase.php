5c44a4fa53921dac004338d66b1a322edf9f4ebf
lenni <lenni.protte@gmx.de>
2025-05-28 15:53:11 +0200

    tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,6 @@ abstract class TestCase extends BaseTestCase
         parent::setUpBeforeClass();
 
         if (!self::$dbConfigLoaded) {
-            echo "Test1\n";
             $projectRoot = dirname(__DIR__);
             $envTestingPath = $projectRoot . '/.env.testing';
             if (!file_exists($envTestingPath)) {
@@ -24,23 +23,21 @@ abstract class TestCase extends BaseTestCase
             }
             self::$dbConfigLoaded = true;
         }
+
+        $appEnv = getenv('APP_ENV');
+        if ($appEnv && $appEnv !== 'testing') {
+            echo
+            "\nWarning:    This test is not running in testing environment and may erase your production database!
+            Set APP_ENV=testing in phpunit.xml/.env.testing or use --env=testing\n";
+        }
     }
 
 
     public function setUp(): void
     {
         parent::setUp();
-        echo "Test2\n";
         if ($this->app->environment() !== 'testing') {
-            echo "\nThis test is not running in testing environment and may erase your production database!\n
-            You can change this either in the phpunit.xml file or by setting the environment variable APP_ENV to testing.\n
-            Do you want to continue anyway? (y/n):\n";
-            $handle = fopen('php://stdin', 'r');
-            $answer = fgets($handle);
-            fclose($handle);
-            if (strtolower(trim($answer)) !== 'y') {
-                $this->markTestSkipped('Test skipped - not running in testing environment');
-            }
+            $this->markTestSkipped('Test skipped - not running in testing environment');
         }
     }
 
