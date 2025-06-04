07437d345303cffd9a4a36809c9244ab3f6a9f5a
leartpro <70581914+leartpro@users.noreply.github.com>
2025-06-04 18:40:17 +0200

    tall-toasts revert

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import "./bootstrap";
 
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import mask from '@alpinejs/mask'
-import ToastComponent from '../../vendor/usernotnull/tall-toasts/resources/js/tall-toasts.js'
-Alpine.data('ToastComponent', ToastComponent)
+import ToastComponent from '../../vendor/usernotnull/tall-toasts/resources/js/tall-toasts'
+Alpine.plugin(ToastComponent)
 Alpine.plugin(mask)
 Livewire.start()
